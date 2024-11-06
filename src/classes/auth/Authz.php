ac2b2be34909e761a2b0e41145243c071da13108
AudinotNoah <37047483+AudinotNoah@users.noreply.github.com>
2024-11-06 15:55:44 +0100

    Update Authz.php

--- src/classes/auth/Authz.php
+++ src/classes/auth/Authz.php
@@ -9,6 +9,7 @@ use iutnc\deefy\exception\AuthnException;
 class Authz {
     public static function checkRole(int $requiredRole): void {
         $user = AuthnProvider::getSignedInUser();
+        echo $user;
         if ($user['role'] !== $requiredRole) {
             throw new AuthnException("Pas acces");
         }
@@ -19,6 +20,7 @@ class Authz {
         $repo = DeefyRepository::getInstance();
 
         $ownerId = $repo->findPlaylistOwner($playlistId);
+        echo $ownerId;
         if ($user['id'] !== $ownerId && $user['role'] !== 100) {
             throw new AuthnException("Vous n'avez pas le droit de voir cette playlist");
         }
