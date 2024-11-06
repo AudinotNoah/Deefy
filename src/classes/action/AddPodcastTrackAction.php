1d2ad1ccc058cce6f2b133f234ab805ce33060a6
AudinotNoah <37047483+AudinotNoah@users.noreply.github.com>
2024-11-06 16:31:43 +0100

    Update AddPodcastTrackAction.php

--- src/classes/action/AddPodcastTrackAction.php
+++ src/classes/action/AddPodcastTrackAction.php
@@ -85,7 +85,6 @@ class AddPodcastTrackAction extends Action{
 
             $repo = DeefyRepository::getInstance();
             $playlistId = $repo->findPlaylistIdByName($PL->nom);
-            echo count($PL->pistes);
             $repo->addTrack($trackData, $playlistId, count($PL->pistes));
 
             $renderer = new AudioListRenderer($PL);
