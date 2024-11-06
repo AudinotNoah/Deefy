7801a93a2b3e12ce8de22a61d61b51098ea89a7c
AudinotNoah <37047483+AudinotNoah@users.noreply.github.com>
2024-11-06 16:29:01 +0100

    Update AddPodcastTrackAction.php

--- src/classes/action/AddPodcastTrackAction.php
+++ src/classes/action/AddPodcastTrackAction.php
@@ -89,6 +89,7 @@ class AddPodcastTrackAction extends Action{
             $repo->addTrack($trackData, $playlistId, count($PL->pistes));
 
             $renderer = new AudioListRenderer($PL);
+            $_SESSION['playlist'] = serialise($PL);
             $playlist_html = $renderer->render(1);
 
 
